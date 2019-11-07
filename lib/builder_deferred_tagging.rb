9337dabf3c6165714160ae6db19faead402f6ccb
ConnorSheremeta <sheremet@ualberta.ca>
2019-11-07 15:07:26 -0700

    Add builder as a dependency and monkey patch into XmlMarkup class

--- lib/builder_deferred_tagging.rb
+++ lib/builder_deferred_tagging.rb
@@ -1,6 +1,17 @@
 require "builder_deferred_tagging/version"
+require 'builder/xmlmarkup'
 
 module BuilderDeferredTagging
-  class Error < StandardError; end
-  # Your code goes here...
+  Builder::XmlMarkup.class_eval do
+    @@deferred_attributes = {}
+    def push_deferred_attribute (attr)
+      @@deferred_attributes = @@deferred_attributes.merge(attr)
+    end
+
+    def deferred_attributes
+      deferred_attributes_copy = @@deferred_attributes
+      @@deferred_attributes = {}
+      deferred_attributes_copy
+    end
+  end
 end
